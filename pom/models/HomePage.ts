eb1aeaf572a33dd1a9113ca20591be4f82e9a579
teddorian <bogdan@onix-systems.com>
2023-03-13 17:11:45 +0200

    fix selectors for mobile

--- pom/models/HomePage.ts
+++ pom/models/HomePage.ts
@@ -11,8 +11,11 @@ export class HomePage {
   readonly tokenScript: Locator;
   readonly m_tokenScript: Locator;
   readonly twitterHeader: Locator;
+  readonly m_twitter: Locator;
   readonly discordHeader: Locator;
+  readonly m_discord: Locator;
   readonly mediumHeader: Locator;
+  readonly m_medium: Locator;
   readonly emailLink: Locator;
   readonly twitterLink: Locator;
   readonly discordLink: Locator;
@@ -37,7 +40,9 @@ constructor(page: Page) {
   this.tokenScript = page.locator("div.hidden.items-center [alt='TokenScript']");
   this.m_tokenScript = page.locator("div.mt-9 a[href='https://www.tokenscript.org/']");
   this.twitterHeader = page.locator('div.hidden a[href="https://twitter.com/TokenScript"]');
+  this.m_twitter = page.locator("div.mb-8 a[href='https://twitter.com/TokenScript']");
   this.discordHeader = page.locator('div.hidden a[href="https://discord.gg/65r8HRBye9"]');
+  this.m_discord = page.locator('div.hidden a[href="https://discord.gg/65r8HRBye9"]');
   this.mediumHeader = page.locator('div.hidden a[href="https://medium.com/alphawallet"]');
   this.emailLink = page.locator('a[href="mailto:sayhi@smarttokenlabs.com"]');
   this.twitterLink = page.locator('a[href="https://twitter.com/TokenScript"].px-11');
@@ -48,6 +53,7 @@ constructor(page: Page) {
   this.twitterFooter = page.locator('div.flex.flex-col a[href="https://twitter.com/TokenScript"]');
   this.discordFooter = page.locator('div.flex.flex-col a[href="https://discord.gg/65r8HRBye9"]');
   this.mediumFooter = page.locator('div.flex.flex-col a[href="https://medium.com/alphawallet"]');
+  this.m_medium = page.locator('div.hidden a[href="https://medium.com/alphawallet"]');
   this.dropdownTriggerSelector = page.locator('div.relative div.flex button');
   this.dropdownItemSelector = page.locator('#routify-app > div:nth-child(2) > div > section > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(2)');
   this.mobileMenu = page.locator('div#menu-icon');
@@ -75,11 +81,19 @@ constructor(page: Page) {
     await this.mobileMenu.click();
     await this.m_browse.click();
     await this.mobileMenu.click();
+    const [newPage] = await Promise.all([
+    this.page.waitForEvent('popup', { timeout: 10000 }),
     await this.m_brandExtender.click(),
+    await this.mobileMenu.click(),
     await this.m_tokenScript.click(),
-    await this.twitterHeader.click(),
-    await this.discordHeader.click(),
-    await this.mediumHeader.click()
+    await this.mobileMenu.click(),
+    await this.m_twitter.click(),
+    await this.mobileMenu.click(),
+    await this.m_discord.click(),
+    await this.mobileMenu.click(),
+    await this.m_medium.click()
+  ]);
+  expect(newPage).toBeTruthy();
   }
   
   async welcomeSectionLinks(){
